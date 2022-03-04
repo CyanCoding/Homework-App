e70960a8c810597a9cb7d3e192eaad5c3c9a9e26
Camden Mac Leod <camden.macleod@pm.me>
2022-03-04 07:19:59 -0800

    First commit

--- Homework-App/MainWindow.xaml.cs
+++ Homework-App/MainWindow.xaml.cs
@@ -13,16 +13,103 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Homework_App
-{
+namespace Homework_App {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+    public partial class MainWindow : Window {
+        public MainWindow() {
             InitializeComponent();
+
+            Style style = new Style();
+            // theme[0]: regular color
+            // theme[1]: selection color
+            string[] theme = Themes.BrushValues[Themes.CurrentTheme].Split(',');
+
+            style.Setters.Add(new Setter(Border.BackgroundProperty, new BrushConverter().ConvertFrom(theme[0]) as Brush));
+            style.Setters.Add(new Setter(Border.BorderBrushProperty, new BrushConverter().ConvertFrom(theme[0]) as Brush));
+            Application.Current.Resources["menu-color"] = style;
+        }
+
+        private void homeworkButton_Click(object sender, RoutedEventArgs e) {
+            //UpdateSelection(homeworkButton);
+        }
+
+        private void classesButton_Click(object sender, RoutedEventArgs e) {
+            //UpdateSelection(classesButton);
+        }
+
+        private void calendarButton_Click(object sender, RoutedEventArgs e) {
+            //UpdateSelection(calendarButton);
+        }
+
+        private void settingsButton_Click(object sender, RoutedEventArgs e) {
+            //UpdateSelection(settingsButton);
+
+            // Prepare the scene
+            themeComboBox.SelectedIndex = Themes.ThemeToInt(Themes.CurrentTheme);
+            settingsGrid.Visibility = Visibility.Visible;
+
+        }
+
+        private void UpdateSelection(Button selectedButton) {
+            Button[] menuList = new Button[] { 
+                homeworkButton,
+                calendarButton,
+                classesButton,
+                settingsButton
+            };
+
+            foreach (Button button in menuList) {
+                if (button == selectedButton) {
+                    Themes.SetSelection(button);
+                }
+                else {
+                    Themes.DeselectButton(button);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Changes the theme globally.
+        /// </summary>
+        private void themeComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            /*            string themeName = Themes.IntTotheme(themeComboBox.SelectedIndex);
+                        Themes.CurrentTheme = themeName;
+
+                        Style style = new Style();
+                        // theme[0]: regular color
+                        // theme[1]: selection color
+                        string[] theme = Themes.BrushValues[themeName].Split(',');
+
+                        style.Setters.Add(new Setter(Border.BackgroundProperty, new BrushConverter().ConvertFrom(theme[0]) as Brush));
+                        style.Setters.Add(new Setter(Border.BorderBrushProperty, new BrushConverter().ConvertFrom(theme[0]) as Brush));
+
+                        Trigger mouseOver = new Trigger() {
+                            //SourceName = "source",
+                            Property = IsMouseOverProperty,
+                            Value = true
+                        };
+
+                        mouseOver.Setters.Add(new Setter(Border.BackgroundProperty, new BrushConverter().ConvertFrom(theme[1]) as Brush));
+                        style.Triggers.Add(mouseOver);*/
+
+            Style style = new Style();
+            style.Setters.Add(new Setter(Button.BackgroundProperty, new BrushConverter().ConvertFrom("#FF0000") as Brush));
+
+            Trigger mouseOver = new Trigger() {
+                Property = IsMouseOverProperty,
+                Value = true
+            };
+
+            mouseOver.Setters.Add(new Setter(Button.BackgroundProperty, new BrushConverter().ConvertFrom("#00FF00") as Brush));
+            style.Triggers.Add(mouseOver);
+
+
+            Application.Current.Resources["menu-color"] = style;
+
+            // We have to update the button selection again for the new style
+            //UpdateSelection(settingsButton);
         }
     }
 }
---
+++ Homework-App/Themes.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Homework_App {
+    internal class Themes {
+        /// <summary>
+        /// Key: The name of the theme
+        /// Value: {normal theme color}, {selection color}
+        /// </summary>
+        internal static Dictionary<string, string> BrushValues = new Dictionary<string, string>() {
+            {"Blueish", "#FFA6C5FF,#FFA6E3FF" },
+            {"Purpleish", "#FFABA6FF,#FFCEA6FF" },
+            {"Redish", "#FFE94B4B,#FFF27B7B"}
+        };
+
+        // We can get the key of BrushValues from this current theme
+        internal static string CurrentTheme = "Blueish";
+
+        /// <summary>
+        /// Sets a button's background to the appropriate color based
+        /// on the currentTheme value.
+        /// </summary>
+        /// <param name="button">The Button to select.</param>
+        internal static void SetSelection(Button button) {
+            // theme[0]: regular color
+            // theme[1]: selection color
+            string[] theme = BrushValues[CurrentTheme].Split(',');
+            button.Background = new BrushConverter().ConvertFrom(theme[1]) as Brush;
+        }
+
+        internal static void DeselectButton(Button button) {
+            // theme[0]: regular color
+            // theme[1]: selection color
+            string[] theme = BrushValues[CurrentTheme].Split(',');
+            button.Background = new BrushConverter().ConvertFrom(theme[0]) as Brush;
+        }
+
+        /// <summary>
+        /// Returns the themeComboBox selection index of the theme
+        /// </summary>
+        /// <param name="value">The theme string</param>
+        /// <returns>An index of the theme in the themeComboBox</returns>
+        internal static int ThemeToInt(string value) {
+            switch (value) {
+                case "Blueish":
+                    return 0;
+                case "Purpleish":
+                    return 1;
+                case "Redish":
+                    return 2;
+
+            }
+            return 0;
+        }
+
+        /// <summary>
+        /// Returns the theme name from the themeComboBox index
+        /// </summary>
+        /// <param name="value">The index of the theme in themeComboBox</param>
+        /// <returns>The name of the theme</returns>
+        internal static string IntTotheme(int value) {
+            switch (value) {
+                case 0:
+                    return "Blueish";
+                case 1:
+                    return "Purpleish";
+                case 2:
+                    return "Redish";
+            }
+            return "Blueish";
+        }
+    }
+}
