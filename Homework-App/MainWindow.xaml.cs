08dc9f122a62df3fd333945b2bbe9c6952351bf4
Camden Mac Leod <camden.macleod@pm.me>
2022-03-11 16:36:26 -0800

    Code assignment creation

--- Homework-App/MainWindow.xaml.cs
+++ Homework-App/MainWindow.xaml.cs
@@ -159,11 +159,13 @@ namespace Homework_App {
         }
 
         private void Label_MouseEnter(object sender, MouseEventArgs e) {
-            testy.Content = "✔";
+            Label box = (Label)sender;
+            box.Content = "✔";
         }
 
         private void Label_MouseLeave(object sender, MouseEventArgs e) {
-            testy.Content = "";
+            Label box = (Label)sender;
+            box.Content = "";
         }
 
         private void cancelButton_Click(object sender, RoutedEventArgs e) {
@@ -262,6 +264,177 @@ namespace Homework_App {
                 // File
                 string filename = dlg.FileName; // System.IO.Path.GetFileName(filename);
             }
+        }
+
+        /// <summary>
+        /// Adds an assignment to the homework tab
+        /// </summary>
+        /// <param name="data">The assignment data from the file</param>
+        private void AddAssignment(Assignment.AssignmentData data) {
+            /*<!-- This is an example homework assignment -->
+            <Grid Height="60" VerticalAlignment="Top">
+                <Grid.ColumnDefinitions>
+                    <ColumnDefinition Width="1*"/>
+                    <ColumnDefinition Width="8*"/>
+                    <ColumnDefinition Width="4*"/>
+                </Grid.ColumnDefinitions>
+                <Grid.RowDefinitions>
+                    <RowDefinition Height="3*"/>
+                    <RowDefinition Height="2*"/>
+                </Grid.RowDefinitions>
+            
+                <!-- Outside border -->
+                <Border Grid.ColumnSpan="3" Grid.RowSpan="3" Margin="0,0,0,-5"  BorderThickness="0,0,0,1" BorderBrush="#FF6C6C6C" Background="White"/>
+                           
+                <!-- Check box -->
+                <Border Grid.Column="0" HorizontalAlignment="Right" Margin="0,5,5,0" Width="18" Height="18" BorderBrush="#FFCA2424" BorderThickness="2">
+                    <Label x:Name="testy" Content="" MouseEnter="Label_MouseEnter" MouseLeave="Label_MouseLeave" FontSize="8" Margin="-3" VerticalAlignment="Center" HorizontalAlignment="Center" Cursor="Hand" Foreground="#FF5B5B5B"/>
+                </Border>
+                           
+                <!-- Assignment name -->
+                <Label FontSize="14" Grid.Column="1" Margin="0,5,0,0" VerticalAlignment="Top" Content="Complete WW2 Assignment"/>
+                            
+                <!-- Due date -->
+                <Label FontSize="13" Grid.Column="2" HorizontalAlignment="Right" VerticalAlignment="Center" Content="Today - 2:00 PM" Foreground="#FF717171"/>
+            
+                <!-- Assignment type -->
+                <Label FontSize="13" Grid.Column="2" Margin="0,-3,0,0" Grid.Row="1" HorizontalAlignment="Right" VerticalAlignment="Center" Content="Presentation" Foreground="#FF717171"/>
+
+                <!-- Class info -->
+                <Grid Grid.Row="1" Grid.Column="1">
+                    <Grid.ColumnDefinitions>
+                        <ColumnDefinition Width="1*"/>
+                        <ColumnDefinition Width="10*"/>
+                    </Grid.ColumnDefinitions>
+                    <Ellipse Margin="5,0,0,0" HorizontalAlignment="Left" VerticalAlignment="Center" Height="10" StrokeThickness="0" Width="10" Fill="#FFF37646"/>
+                    <Label FontSize="13" Margin="-5,-3,0,0" VerticalContentAlignment="Center" HorizontalAlignment="Left" Grid.Column="1" Content="American History"/>
+                </Grid>
+             </Grid>*/
+
+            // Outer grid
+            Grid outerGrid = new Grid();
+            outerGrid.Height = 60;
+            outerGrid.VerticalAlignment = VerticalAlignment.Top;
+
+            ColumnDefinition column1 = new ColumnDefinition();
+            column1.Width = new GridLength(1, GridUnitType.Star);
+            ColumnDefinition column2 = new ColumnDefinition();
+            column2.Width = new GridLength(8, GridUnitType.Star);
+            ColumnDefinition column3 = new ColumnDefinition();
+            column3.Width = new GridLength(4, GridUnitType.Star);
+
+            RowDefinition row1 = new RowDefinition();
+            row1.Height = new GridLength(3, GridUnitType.Star);
+            RowDefinition row2 = new RowDefinition();
+            row2.Height = new GridLength(2, GridUnitType.Star);
+
+            outerGrid.ColumnDefinitions.Add(column1);
+            outerGrid.ColumnDefinitions.Add(column2);
+            outerGrid.ColumnDefinitions.Add(column3);
+
+            outerGrid.RowDefinitions.Add(row1);
+            outerGrid.RowDefinitions.Add(row2);
+
+            // Outside border
+            Border outsideBorder = new Border();
+            outsideBorder.SetValue(Grid.ColumnSpanProperty, 3);
+            outsideBorder.SetValue(Grid.RowSpanProperty, 3);
+            outsideBorder.Margin = new Thickness(0, 0, 0, -5);
+            outsideBorder.BorderThickness = new Thickness(0, 0, 0, 1);
+            outsideBorder.BorderBrush = new BrushConverter().ConvertFrom("#FF6C6C6C") as Brush;
+            outsideBorder.Background = new SolidColorBrush(Colors.White);
+            outerGrid.Children.Add(outsideBorder);
+
+            // Check box
+            Border checkboxBorder = new Border();
+            checkboxBorder.HorizontalAlignment = HorizontalAlignment.Left;
+            checkboxBorder.Margin = new Thickness(0, 5, 5, 0);
+            checkboxBorder.Width = 18;
+            checkboxBorder.Height = 18;
+            // TODO: Set priority color here
+            checkboxBorder.BorderBrush = new BrushConverter().ConvertFrom("#FFCA2424") as Brush;
+            checkboxBorder.BorderThickness = new Thickness(2);
+
+            Label checkboxLabel = new Label();
+            checkboxLabel.MouseEnter += Label_MouseEnter;
+            checkboxLabel.MouseLeave += Label_MouseLeave;
+            checkboxLabel.FontSize = 8;
+            checkboxLabel.Margin = new Thickness(-3);
+            checkboxLabel.VerticalAlignment = VerticalAlignment.Center;
+            checkboxLabel.HorizontalAlignment = HorizontalAlignment.Center;
+            checkboxLabel.Cursor = Cursors.Hand;
+            checkboxLabel.Foreground = new BrushConverter().ConvertFrom("#FF5B5B5B") as Brush;
+
+            checkboxBorder.Child = checkboxLabel;
+            outerGrid.Children.Add(checkboxBorder);
+
+            // Assignment name
+            Label assignmentName = new Label();
+            assignmentName.FontSize = 14;
+            assignmentName.SetValue(Grid.ColumnProperty, 1);
+            assignmentName.Margin = new Thickness(0, 5, 0, 0);
+            assignmentName.VerticalAlignment = VerticalAlignment.Top;
+            assignmentName.Content = ""; // TODO: Fill in title here
+            outerGrid.Children.Add(assignmentName);
+
+            // Due date
+            Label dueDate = new Label();
+            dueDate.FontSize = 13;
+            dueDate.SetValue(Grid.ColumnProperty, 3);
+            dueDate.HorizontalAlignment = HorizontalAlignment.Right;
+            dueDate.VerticalAlignment = VerticalAlignment.Center;
+            dueDate.Content = ""; // TODO: Fill in due date content
+            dueDate.Foreground = new BrushConverter().ConvertFrom("#FF717171") as Brush;
+            outerGrid.Children.Add(dueDate);
+
+            // Assignment type
+            Label type = new Label();
+            type.FontSize = 13;
+            type.SetValue(Grid.ColumnProperty, 2);
+            type.SetValue(Grid.RowProperty, 1);
+            type.HorizontalAlignment = HorizontalAlignment.Right;
+            type.VerticalAlignment = VerticalAlignment.Center;
+            type.Margin = new Thickness(0, -3, 0, 0);
+            type.Content = ""; // TODO: Fill in type content here
+            type.Foreground = new BrushConverter().ConvertFrom("#FF717171") as Brush;
+            outerGrid.Children.Add(type);
+
+            // Class info
+            Grid classGrid = new Grid();
+            classGrid.SetValue(Grid.RowProperty, 1);
+            classGrid.SetValue(Grid.ColumnProperty, 1);
+
+            ColumnDefinition classColumn1 = new ColumnDefinition();
+            column1.Width = new GridLength(1, GridUnitType.Star);
+            ColumnDefinition classColumn2 = new ColumnDefinition();
+            column1.Width = new GridLength(10, GridUnitType.Star);
+
+            classGrid.ColumnDefinitions.Add(classColumn1);
+            classGrid.ColumnDefinitions.Add(classColumn2);
+
+            Ellipse classEllipse = new Ellipse();
+            classEllipse.Width = 10;
+            classEllipse.Height = 10;
+            classEllipse.Margin = new Thickness(5, 0, 0, 0);
+            classEllipse.HorizontalAlignment = HorizontalAlignment.Left;
+            classEllipse.VerticalAlignment = VerticalAlignment.Center;
+            classEllipse.StrokeThickness = 0;
+            // TODO: Get class color for ellipse
+            classEllipse.Fill = new BrushConverter().ConvertFrom("#FFF37646") as Brush;
+            classGrid.Children.Add(classEllipse);
+
+            Label classLabel = new Label();
+            classLabel.FontSize = 13;
+            classLabel.Margin = new Thickness(-5, -3, 0, 0);
+            classLabel.HorizontalAlignment = HorizontalAlignment.Left;
+            classLabel.VerticalAlignment = VerticalAlignment.Center;
+            classLabel.SetValue(Grid.ColumnProperty, 1);
+            classLabel.Content = ""; // TODO: Get class name here
+            classGrid.Children.Add(classLabel);
+
+            outerGrid.Children.Add(classGrid);
+
+            dueTodayScrollview.Content = outerGrid;
         }
     }
 }
