77cf26f236563c9e77435ea8623c84ec120f2b19
Camden Mac Leod <camden.macleod@pm.me>
2022-03-19 07:45:45 -0400

    Add ClassData struct

---
+++ Homework-App/ClassData.cs
@@ -0,0 +1,16 @@
+﻿namespace Homework_App; 
+
+public struct ClassData {
+    public string Name;
+    public string Building;
+    public string Room;
+    public string Professor;
+    public string Pronouns;
+    public string StartDate;
+    public string EndDate;
+    public string Time;
+    public string Number;
+    public bool[] DaysEachWeek;
+    public string Reminder;
+    public string Notes;
+}
