fd202fef1ee2edeeed6f03a2cdf87abf3d1ad32e
Camden Mac Leod <camden.macleod@pm.me>
2022-03-24 14:53:25 -0400

    Create ClassManifest
    This is used for reading JSON class values into C# objects

---
+++ Homework-App/ClassManifest.cs
@@ -0,0 +1,17 @@
+﻿namespace Homework_App; 
+
+internal class ClassManifest {
+    public string Name { get; set; }
+    public string Building { get; set; }
+    public string Room { get; set; }
+    public string Professor { get; set; }
+    public string Pronouns { get; set; }
+    public string StartDate { get; set; }
+    public string EndDate { get; set; }
+    public string Time { get; set; }
+    public string Number { get; set; }
+    public string Color { get; set; }
+    public string Reminder { get; set; }
+    public string Notes { get; set; }
+    public bool?[] DaysEachWeek { get; set; }
+}
