92be941ed54e26a992097846b3530807dbd305e4
Camden Mac Leod <camden.macleod@pm.me>
2022-04-09 05:10:32 -0400

    Replace control with UserControl

--- Homework-App/Pages/Test.xaml.cs
+++ Homework-App/Pages/Test.xaml.cs
@@ -12,33 +12,4 @@ public partial class Test : Window {
     public Test() {
         InitializeComponent();
     }
-
-    private void BorderMouseEnter(object sender, MouseEventArgs e) {
-        menuBorder.BeginAnimation(UIElement.OpacityProperty,
-            new DoubleAnimation(1d, TimeSpan.FromSeconds(0.25)));
-
-        colorBorder.BeginAnimation(Ellipse.WidthProperty,
-            new DoubleAnimation(35d, TimeSpan.FromSeconds(0.4)));
-        
-        colorBorder.BeginAnimation(Ellipse.HeightProperty,
-            new DoubleAnimation(35d, TimeSpan.FromSeconds(0.4)));
-        
-        colorBorder.BeginAnimation(Ellipse.OpacityProperty,
-            new DoubleAnimation(0d, TimeSpan.FromSeconds(0.8)));
-    }
-
-
-    private void BorderMouseLeave(object sender, MouseEventArgs e) {
-        menuBorder.BeginAnimation(UIElement.OpacityProperty,
-            new DoubleAnimation(0d, TimeSpan.FromSeconds(0.25)));
-        
-        colorBorder.BeginAnimation(Ellipse.WidthProperty,
-            new DoubleAnimation(0d, TimeSpan.FromSeconds(0.4)));
-        
-        colorBorder.BeginAnimation(Ellipse.HeightProperty,
-            new DoubleAnimation(0d, TimeSpan.FromSeconds(0.4)));
-        
-        colorBorder.BeginAnimation(Ellipse.OpacityProperty,
-            new DoubleAnimation(0.45d, TimeSpan.FromSeconds(0.3)));
-    }
 }
