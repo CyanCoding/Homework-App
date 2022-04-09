1fb660332db197c19e4d3f8fdfa6097e149dc35e
Camden Mac Leod <camden.macleod@pm.me>
2022-04-08 22:35:09 -0400

    Add button hover feature

--- Homework-App/Pages/Test.xaml.cs
+++ Homework-App/Pages/Test.xaml.cs
@@ -1,9 +1,44 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
 
 namespace Homework_App.Pages; 
 
 public partial class Test : Window {
     public Test() {
         InitializeComponent();
+    }
+
+    private void BorderMouseEnter(object sender, MouseEventArgs e) {
+        menuBorder.BeginAnimation(UIElement.OpacityProperty,
+            new DoubleAnimation(1d, TimeSpan.FromSeconds(0.25)));
+
+        colorBorder.BeginAnimation(Ellipse.WidthProperty,
+            new DoubleAnimation(35d, TimeSpan.FromSeconds(0.4)));
+        
+        colorBorder.BeginAnimation(Ellipse.HeightProperty,
+            new DoubleAnimation(35d, TimeSpan.FromSeconds(0.4)));
+        
+        colorBorder.BeginAnimation(Ellipse.OpacityProperty,
+            new DoubleAnimation(0d, TimeSpan.FromSeconds(0.8)));
+    }
+
+
+    private void BorderMouseLeave(object sender, MouseEventArgs e) {
+        menuBorder.BeginAnimation(UIElement.OpacityProperty,
+            new DoubleAnimation(0d, TimeSpan.FromSeconds(0.25)));
+        
+        colorBorder.BeginAnimation(Ellipse.WidthProperty,
+            new DoubleAnimation(0d, TimeSpan.FromSeconds(0.4)));
+        
+        colorBorder.BeginAnimation(Ellipse.HeightProperty,
+            new DoubleAnimation(0d, TimeSpan.FromSeconds(0.4)));
+        
+        colorBorder.BeginAnimation(Ellipse.OpacityProperty,
+            new DoubleAnimation(0.45d, TimeSpan.FromSeconds(0.3)));
     }
 }
